dde1682856481cb4bd131945190ff21a13942335
Daniel Broadhead <broadhead.daniel7@gmail.com>
2026-07-22 18:06:32 -0600

    Troubleshooting memory problems, init random forest

--- src/features.py
+++ src/features.py
@@ -6,7 +6,7 @@ import numpy as np
 def smiles_to_fp(smiles, radius=2, n_bits=2048):
     mol = Chem.MolFromSmiles(smiles)
     if mol is None:
-        return np.zeros(n_bits)  # handle invalid SMILES
+        return np.zeros(n_bits, dtype=np.uint8)  # handle invalid SMILES
     fp = AllChem.GetMorganFingerprintAsBitVect(mol, radius, nBits=n_bits)
-    return np.array(fp)
+    return np.array(fp, dtype=np.uint8)
     
